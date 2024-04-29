65bc0969ead6f2ea68cab75721e601e8dcb7485c
cosmas cheruiyot <cosmas@cosmas.cossi>
2024-04-29 21:10:19 +0300

    phewx

--- app/Auth/page.tsx
+++ app/Auth/page.tsx
@@ -2,8 +2,7 @@ import Signup from '@/components/Signup'
 export default function Home() {
     return (
         <div>
-        <h1>Home</h1>
-       <Signup/>
+        <Signup/>
         </div>
     );
     }
